278e1fef6da7eae204085e440bc7000a037eadaa
Marco <marco.a.formoso@gmail.com>
2017-12-31 18:47:12 +0100

    HasAuthority

---
+++ Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MovingPlatform.h"
+
+
+AMovingPlatform::AMovingPlatform() {
+
+	PrimaryActorTick.bCanEverTick = true;
+	SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (HasAuthority())	SetActorLocation(GetActorLocation() + FVector(DeltaTime * Speed, 0, 0));
+}
